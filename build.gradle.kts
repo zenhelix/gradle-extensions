f59d2eea1da004714ba252802b5647b9e000856a
zenhelix-dev <dm.medakin.online@gmail.com>
2025-05-01 19:32:50 +0400

    init (#1)

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+import org.gradle.api.internal.tasks.JvmConstants
+
+plugins {
+    id("io.github.zenhelix.kotlin-jvm-library") apply false
+    id("io.github.zenhelix.maven-central-publish")
+    `java-library`
+}
+
+allprojects {
+    group = "io.github.zenhelix"
+}
+
+configure(subprojects.filter { it.childProjects.isEmpty() }) {
+    apply(plugin = "io.github.zenhelix.maven-central-publish")
+
+    publishing {
+        repositories {
+            mavenLocal()
+            mavenCentralPortal {
+                uploader { maxRetriesStatusCheck = 10 }
+                credentials {
+                    username = System.getProperty("MAVEN_SONATYPE_USERNAME") ?: System.getenv("MAVEN_SONATYPE_USERNAME")
+                    password = System.getProperty("MAVEN_SONATYPE_TOKEN") ?: System.getenv("MAVEN_SONATYPE_TOKEN")
+                }
+            }
+        }
+    }
+
+    signing {
+        val signingKeyId: String? by project
+        val signingKey: String? by project
+        val signingPassword: String? by project
+
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+        sign(publishing.publications)
+    }
+
+    publishing.publications.withType<MavenPublication> {
+        pom {
+            description = "Helpers for Gradle"
+            url = "https://github.com/zenhelix/gradle-extensions"
+            licenses {
+                license {
+                    name = "The Apache License, Version 2.0"
+                    url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                }
+            }
+            scm {
+                connection = "scm:git:git://github.com/zenhelix/gradle-extensions.git"
+                developerConnection = "scm:git:ssh://github.com/zenhelix/gradle-extensions.git"
+                url = "https://github.com/zenhelix/gradle-extensions"
+            }
+            developers {
+                developer {
+                    id = "dm.medakin"
+                    name = "Dmitrii Medakin"
+                    email = "dm.medakin.online@gmail.com"
+                }
+            }
+        }
+    }
+}
+
+configure(subprojects.filter { it.name.contains("gradle-extensions-platform-").not() }) {
+    apply(plugin = "io.github.zenhelix.kotlin-jvm-library")
+
+    publishing {
+        publications {
+            create<MavenPublication>("java") {
+                from(components.getByName(JvmConstants.JAVA_MAIN_COMPONENT_NAME))
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,58 @@
+@file:Suppress("UnstableApiUsage")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "gradle-extensions"
+
+project("gradle-extensions-platform") {
+    include("gradle-extensions-platform-bom")
+    include("gradle-extensions-platform-toml")
+}
+include("gradle-utils")
+project("gradle-test") {
+    include("gradle-test-all")
+    include("gradle-test-kit-utils")
+    include("gradle-assertions")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+        mavenLocal()
+    }
+}
+
+private fun Settings.project(
+    baseProject: String, initializer: IncludeContext.() -> Unit = {}
+): IncludeContext = IncludeContext(baseProject, this).apply(initializer)
+
+private class IncludeContext(private val baseProject: String, private val delegate: Settings) {
+
+    fun project(
+        baseProject: String, initializer: IncludeContext.() -> Unit = {}
+    ): IncludeContext = IncludeContext("${this.baseProject}:$baseProject", this.delegate).apply(initializer)
+
+    fun include(vararg project: String) {
+        project.forEach {
+            delegate.include("$baseProject:$it")
+        }
+    }
+
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenLocal()
+    }
+
+    val zenhelixGradleVersion: String by settings
+    val mavenCentralPublishVersion: String by settings
+
+    plugins {
+        id("io.github.zenhelix.maven-central-publish") version mavenCentralPublishVersion
+        id("io.github.zenhelix.kotlin-jvm-library") version zenhelixGradleVersion
+    }
+}
